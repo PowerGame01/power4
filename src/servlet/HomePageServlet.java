c7374a737118ac3aba9ae7325059febe14c30764
flavien <flavien@flavien-Desktop>
2018-03-29 16:39:55 +0200

    test

--- src/servlet/HomePageServlet.java
+++ src/servlet/HomePageServlet.java
@@ -14,15 +14,11 @@ import javax.servlet.http.HttpServletResponse;
 public class HomePageServlet extends HttpServlet{
 
 	@Override
-<<<<<<< HEAD
+
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		
-
-=======
-	protected void service(HttpServletRequest req, HttpServletResponse res)
-	throws ServletException, IOException {
 		req.getParameter("nom");
->>>>>>> branch 'master' of https://github.com/PowerGame01/power4.git
+		
 	}
 	
 }
