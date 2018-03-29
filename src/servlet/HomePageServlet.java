293dce8c637698e180639be5c40daaefc4d4b835
Alexandre BRIX-FELICIAGGI <alexandrebrixfeliciaggi@gmail.com>
2018-03-29 15:54:59 +0200

    build

---
+++ src/servlet/HomePageServlet.java
@@ -0,0 +1,21 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HomePage")
+
+public class HomePageServlet extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		
+	}
+
+}
