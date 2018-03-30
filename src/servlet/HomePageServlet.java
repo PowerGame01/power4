55d457b0358c90fa97c2b27d499b41b04bdabf26
pierrot <pierrot@DESKTOP-5Q1PFKU>
2018-03-30 15:56:30 +0200

    update le homepage, attention probleme pour se connecter ...

--- src/servlet/HomePageServlet.java
+++ src/servlet/HomePageServlet.java
@@ -8,6 +8,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import beans.Player;
+import connectionDAO.MySQLConnection;
+import dao.PlayerDAO;
+
 @SuppressWarnings("serial")
 @WebServlet("/home")
 
@@ -16,9 +20,27 @@ public class HomePageServlet extends HttpServlet{
 	@Override
 
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-
 		
-		req.getParameter("nom");
+		new MySQLConnection();
+		
+		String name = req.getParameter("name");
+		//lancer la fonction DAO qui recherche le nom de notre input.
+		Player playerExistant = new PlayerDAO().findByNameWaiting(name);
+	
+		
+		if (playerExistant !=null)
+			// call the function waitingRoom.
+			// waitingRoom(new playerDAO());
+		{
+			playerExistant.setWaiting(false); // update player
+			new PlayerDAO().update(playerExistant); // passe l utilisateur sur la db en mode busy
+	}
+		else
+		{		//waitingRoom(playerA).
+			Player playerNew = new Player(0,name,true);
+			new PlayerDAO().create(playerNew);
+		}
+		
 		
 	}
 	
