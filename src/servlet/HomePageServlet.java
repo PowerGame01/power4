3d205b0e81e1d08ac7a1051d106082fd0473f123
Vincent <vincentromand83@gmail.com>
2018-03-30 08:35:43 +0200

    modification du nom de la webServlet en /home

--- src/servlet/HomePageServlet.java
+++ src/servlet/HomePageServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
-@WebServlet("/HomePage")
+@WebServlet("/home")
 
 public class HomePageServlet extends HttpServlet{
 
