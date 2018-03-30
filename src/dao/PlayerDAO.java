d69bbd338a017ffa83a7aee597c13ac8cd0a2885
Vincent <vincentromand83@gmail.com>
2018-03-30 14:14:15 +0200

    Création du fichier PlayerDAO

---
+++ src/dao/PlayerDAO.java
@@ -0,0 +1,92 @@
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import beans.Player;
+
+public class PlayerDAO extends DAO<Player>{
+	
+	private final String TABLE = "player";
+	@Override
+	public Player find(Integer id) {
+		Player player = null;
+		try {
+			String request = "SELECT * FROM " + TABLE + " WHERE id = ?";
+			PreparedStatement ps = this.connection.prepareStatement(request,
+            Statement.RETURN_GENERATED_KEYS);
+			ps.setLong(1, id);
+			ResultSet result = ps.executeQuery();
+			if (result.first()) {
+                player = new Player(
+                        id,
+                        result.getString("name"),
+                        result.getBoolean("waiting")
+                );
+            }
+		}catch (SQLException ex){
+			Logger.getLogger(PlayerDAO.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return player;
+	}
+
+	@Override
+	public Player create(Player obj) {
+		try {
+			String request = "INSERT INTO" + TABLE + "(name,waiting) VALUES (?,?)";
+			PreparedStatement ps = this.connection.prepareStatement(request,Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, obj.getName());
+			ps.setBoolean(2, obj.isWaiting());
+			ResultSet rs = ps.getGeneratedKeys();
+            int last_inserted_id;
+            if (rs.first()) { // Si on a des id créés on lit le premier
+                last_inserted_id = rs.getInt(1);
+                // On récupère l'enregistrement créé
+                obj = this.find(last_inserted_id);
+            }
+		}catch (SQLException ex) {
+			Logger.getLogger(PlayerDAO.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return obj;
+	}
+
+	@Override
+	public Player update(Player obj) {
+		try {
+			String request = "UPDATE " + TABLE + "WHERE id = ?";
+			PreparedStatement ps = this.connection.prepareStatement(request);
+			ps.setBoolean(3, obj.isWaiting());
+		}catch (Exception ex) {
+			Logger.getLogger(PlayerDAO.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return obj;
+	}
+
+	@Override
+	public void delete(Player obj) {
+		try {
+            String request = "DELETE FROM " + TABLE + " WHERE id = ?";
+            PreparedStatement ps = this.connection.prepareStatement(request);
+            ps.setLong(1, obj.getId());
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(PlayerDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+	}
+	
+	public Player findByNameWaiting(String name) {
+		Player player = null;
+		try {
+			String request = "SELECT * FROM" + TABLE +"WHERE name !="+name+"AND waiting = true";
+			Object req = request;
+			player = (Player) req;
+		} catch (Exception ex) {
+			Logger.getLogger(PlayerDAO.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return player;
+	}
+	
+}
