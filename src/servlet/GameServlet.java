b9e3ff8dc6d39e72decccad66909f2e02943c3b1
Alexandre BRIX-FELICIAGGI <alexandrebrixfeliciaggi@gmail.com>
2018-04-01 22:01:08 +0200

    Receiving the column and the row of the jQuery to send to database.

--- src/servlet/GameServlet.java
+++ src/servlet/GameServlet.java
@@ -15,7 +15,10 @@ public class GameServlet extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
-
+		int col = Integer.parseInt(req.getParameter("col"));
+		System.out.println("col : "+col);
+		int row = Integer.parseInt(req.getParameter("row"));
+		System.out.println("row : "+row);
 	}
 
 	
