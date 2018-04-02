fd4f9a69d5751adb446b2b49ca39673d5beb9e5c
Alexandre BRIX-FELICIAGGI <alexandrebrixfeliciaggi@gmail.com>
2018-04-02 18:36:49 +0200

    Began creating my CRUD

--- src/dao/GameDAO.java
+++ src/dao/GameDAO.java
@@ -2,6 +2,10 @@ package dao;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import beans.Game;
 
@@ -11,7 +15,7 @@ private final String TABLE = "game";
 	public Game find(Integer id) {
 		Game game = null;
 		try {
-			String request = "SELECT* FROM" + TABLE + "WHERE id = ?";
+			String request = "SELECT* FROM " + TABLE + " WHERE id = ?";
 			PreparedStatement ps = this.connection.prepareStatement(request);
 			ps.setLong(1, id);
             ResultSet result = ps.executeQuery();
@@ -29,9 +33,27 @@ private final String TABLE = "game";
 	}
 
 	@Override
-	public Game create(Game obj) {
-		// TODO Auto-generated method stub
-		return null;
+	public Game create(Game game) {
+		try {
+			//System.out.println(this.connection.isClosed());
+			String request = "INSERT INTO " + TABLE + " (id_player,id_player_1) VALUES (?,?)";
+//			System.out.println("PlayerDAO before ps");
+			PreparedStatement ps = this.connection.prepareStatement(request, Statement.RETURN_GENERATED_KEYS);
+			System.out.println(game.toString());
+			ps.setInt(1, game.getId_player());
+			ps.setInt(2, game.getId_player());
+			ps.executeUpdate();
+			ResultSet rs = ps.getGeneratedKeys();
+            int last_inserted_id;
+            if (rs.first()) { // Si on a des id créés on lit le premier
+                last_inserted_id = rs.getInt(1);
+                // On récupère l'enregistrement créé
+                game = this.find(last_inserted_id);
+            }
+		}catch (SQLException ex) {
+			Logger.getLogger(GameDAO.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return game;
 	}
 
 	@Override
@@ -41,9 +63,15 @@ private final String TABLE = "game";
 	}
 
 	@Override
-	public void delete(Game obj) {
-		// TODO Auto-generated method stub
-		
+	public void delete(Game game) {
+		try {
+            String request = "DELETE FROM " + TABLE + " WHERE id = ?";
+            PreparedStatement ps = this.connection.prepareStatement(request);
+            ps.setLong(1, game.getId());
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(GameDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
 	}
 
 }
