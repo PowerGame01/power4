4be9617cda86d5d87780f5c287d3d51d2d7839f9
Alexandre BRIX-FELICIAGGI <alexandrebrixfeliciaggi@gmail.com>
2018-04-02 18:38:20 +0200

    Tried to send everything to my jsp

--- src/servlet/GameServlet.java
+++ src/servlet/GameServlet.java
@@ -15,7 +15,7 @@ import dao.DAO;
 import dao.DAOFactory;
 import dao.PositionDAO;
 
-@WebServlet("/power4")
+@WebServlet("/game")
 @SuppressWarnings("serial")
 public class GameServlet extends HttpServlet{
 
@@ -30,25 +30,19 @@ public class GameServlet extends HttpServlet{
 		System.out.println("col : "+col);
 		int row = Integer.parseInt(req.getParameter("row"));
 		System.out.println("row : "+row);
-		
-		/*Position positionNew = new Position(0,row,col,playerNew.getId());
-		
-		new PositionDAO().create(positionNew);
-		*/
+
 		try {
-			//System.out.println("start");
 			Class.forName("com.mysql.jdbc.Driver");
 			DAO<Position> positionDAO = DAOFactory.getPositionDAO();
-			//System.out.println("after DAO");
-			Position test = new Position(0,row,col,1);
-			//System.out.println(test.toString());
-//			Player test2 = new Player("Jean", true);
+			Position test = new Position(0,row,col,2);
 			test = positionDAO.create(test);
 			
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		req.getRequestDispatcher("/game.jsp").forward(req, res);
 	}
 	
 }
