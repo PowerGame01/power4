c7683ab3f0075f08d79a071eb6e7959b4a2484b3
Vincent <vincentromand83@gmail.com>
2018-04-04 11:08:57 +0200

    delete comment

--- src/servlet/WaitingRoomServlet.java
+++ src/servlet/WaitingRoomServlet.java
@@ -31,38 +31,26 @@ public class WaitingRoomServlet extends HttpServlet {
 			Class.forName("com.mysql.jdbc.Driver");
 			playerDAO = DAOFactory.getPlayerDAO();
 		} catch (ClassNotFoundException e1) {
-			// TODO Auto-generated catch block
+			
 			e1.printStackTrace();
 		}
-		
-//		Player test = new Player("jhon",false);
-//		System.out.println("test = " + test.toString());
-//		test = playerDAO.create(test);
-//		System.out.println("test = " + test);
-		
-//		System.out.println("opponent = " + session.getAttribute("opponent"));
-//		System.out.println("player = " + session.getAttribute("player"));
+
 		if(session.getAttribute("opponent") != null && session.getAttribute("player") != null) {
 			req.setAttribute("player", session.getAttribute("player"));
 			req.setAttribute("opponent", session.getAttribute("opponent"));
 			req.getRequestDispatcher("/game").forward(req, res);
 		}else {
 			if(session.getAttribute("player") != null) {
-//				System.out.println("nom du joueur = " + player.getName());
+
 				opponent =  ((PlayerDAO) playerDAO).findOpponent(((Player) session.getAttribute("player")).getName());
 				session.setAttribute("opponent", ((Player)opponent));
-				
-//				System.out.println("nom de l'adversaire = " + opponent.getName());
 				System.out.println(((PlayerDAO) playerDAO).findOpponent(session.getAttribute("player").toString()));
 				if(session.getAttribute("opponent") == null) {
 					req.setAttribute("opponent", session.getAttribute("opponent"));
-					req.getRequestDispatcher("/waitingRoom.jsp").forward(req, res);
+					req.getRequestDispatcher("/WEB-INF/views/waitingRoom.jsp").forward(req, res);
 				}else req.getRequestDispatcher("/game").forward(req, res);
 			}else {
 				try {
-//					Class.forName("com.mysql.jdbc.Driver");
-//					playerDAO = DAOFactory.getPlayerDAO();
-					System.out.println("Coucou du try player");
 					player = new Player(req.getParameter("name"), true);
 					player = playerDAO.create(player);
 					session.setAttribute("player", ((Player)player));
